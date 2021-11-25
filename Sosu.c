40fdaebdcc1842291c3939b475d71f5081608300
한예찬 <pantajia12@naver.com>
2021-11-25 22:59:22 +0900

    20211114

--- Sosu.c
+++ Sosu.c
@@ -2,34 +2,34 @@
 #include <time.h>
 
 int main() {
-   
    int num = 0;
-   scanf("%d", &num);//10을 입력했는데 소수가 아닌 10이 출력됨 왜지  
+   scanf("%d", &num);//259051 까지 검색가능 
    
-   clock_t start = clock();
    int Sosu[num];
    int Sosu_index[num] = {0};
    
-   int i;//왜 registerd int가 int보다 느릴까 
+   int i;//왜 register int가 int보다 느릴까 
+   clock_t start = clock();
+   
    for(i=0;i<=num;i++) {
+      Sosu[i] = i;
       Sosu_index[i] = 2;
    }
    
    for(i=2;i<=num;i++) {
-      Sosu[i] = i;
       if(Sosu_index[i] == 2) {
          Sosu_index[i] = 1;
          int a = i;
-         while(a < num) {
+         while(a <= num) {
             a += i;
-            if(a < num)
+            if(a <= num)
                Sosu_index[a] = 0;
          }
       }
    }
-   
-   
-   
+   Sosu_index[0] = 0;
+   Sosu_index[1] = 0;
+      
    for(i=0;i<=num;i++) {
       if(Sosu_index[i] == 1)
          printf("%d\n", Sosu[i]);
